f6a90655187642a297aa7649181edf0c832c0b8b
OscarIsorez <91247570+OscarIsorez@users.noreply.github.com>
2024-04-20 11:49:59 +0200

    Remove commented out code in main.dart

---
+++ lib/ConvTheme.dart
@@ -0,0 +1,13 @@
+class ConvTheme {
+  final String name;
+  final String sysPrompt;
+
+  ConvTheme({required this.name, required this.sysPrompt});
+
+  @override
+  String toString() {
+    return name;
+  }
+
+  
+}
