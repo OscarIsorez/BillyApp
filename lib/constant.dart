de241373957beab5636efbc8f1b75738d37ba3c2
OscarIsorez <91247570+OscarIsorez@users.noreply.github.com>
2024-04-24 11:54:55 +0200

    Update conversation prompt and fix bug in Page2.dart

--- lib/constant.dart
+++ lib/constant.dart
@@ -3,19 +3,12 @@ import 'package:billy/templates/ConversationType.dart';
 import 'package:flutter/material.dart';
 
 class Constants {
-  static const int maxAttempts = 3;
-  static const double pi = 3.14159;
-  static const String appName = "MyApp";
-  // ignore: non_constant_identifier_names
+
   static const SenderColorMessage = Color(0xFF5381A7);
   // ignore: non_constant_identifier_names
   static const ReceiverColorMessage = Color.fromARGB(255, 212, 111, 111);
 
-  static const List<String> themes = [
-    'Theme 1',
-    'Theme 2',
-    'Theme 3',
-  ];
+
 
   static const correctionSystemPrompt =
       """In this task, your goal is to generate a corrected version of a message that will be concatenated to these instructions.
